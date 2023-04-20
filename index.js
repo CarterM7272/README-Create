0fa5ae43eb69f727d266aa4e4d3e5a0bbcfc2803
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-04-20 00:43:34 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  "What is your Github username?",
+  "What is your email address?",
+  "What is your project's name?",
+  "Please write a short description of your project",
+  "What kind of license should your project have?",
+  "What command should be run to install dependencies"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
